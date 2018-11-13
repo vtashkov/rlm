60cafd59af4c3e9160db0668cb4c4157744f2ff9
Victor Tashkov <vtashkov@gmail.com>
2018-11-13 12:33:55 +0200

    adding arrayfire dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
+
 #[cfg(test)]
 mod tests {
+
+    extern crate arrayfire as af;
+
+    use self::af::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        info();
+        assert_eq!(2, 2);
     }
 }
