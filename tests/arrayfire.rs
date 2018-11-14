8552cb26a3dfcdb2905baffc6fd467aa3f18a442
Victor Tashkov <vtashkov@gmail.com>
2018-11-14 22:31:33 +0200

    test added for creation of array with n constant values

--- tests/arrayfire.rs
+++ tests/arrayfire.rs
@@ -4,9 +4,22 @@ use arrayfire::*;
 
 #[test]
 fn can_create_array_with_one_constant_value() {
-    let value = 0;
+    let value = 1;
     let a = constant(value, Dim4::new(&[1, 1, 1, 1]));
     let mut b = &mut vec![0];
     a.host(&mut b);
     assert_eq!(value, b[0]);
+}
+
+#[test]
+fn can_create_array_with_n_constant_values() {
+    let n = 3;
+    let value = 1;
+    let a = constant(value, Dim4::new(&[n as u64, 1, 1, 1]));
+    let mut b = &mut vec![0; n];
+    a.host(&mut b);
+
+    let mut expected = &mut vec![1, 1, 1];
+    assert_eq!(n, b.len());
+    assert_eq!(expected, b);
 }
