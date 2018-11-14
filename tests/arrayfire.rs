ed4b62a71a439b443998898c153424a5f1b9ae6c
Victor Tashkov <vtashkov@gmail.com>
2018-11-15 00:36:36 +0200

    tests refactored to have MxN notation

--- tests/arrayfire.rs
+++ tests/arrayfire.rs
@@ -4,13 +4,13 @@ use arrayfire::*;
 
 #[test]
 fn can_get_empty_array_dimensions() {
-    let n = 3;
-    let m = 5;
-    let gpu_array = Array::<f64>::new_empty(Dim4::new(&[n, m, 1, 1]));
+    let m = 3;
+    let n = 5;
+    let gpu_array = Array::<f64>::new_empty(Dim4::new(&[m, n, 1, 1]));
     let dim = gpu_array.dims();
 
-    assert_eq!(n, dim[0]);
-    assert_eq!(m, dim[1]);
+    assert_eq!(m, dim[0]);
+    assert_eq!(n, dim[1]);
     assert_eq!(1, dim[2]);
     assert_eq!(1, dim[3]);
 }
@@ -21,6 +21,7 @@ fn can_create_array_with_one_constant_integer_value() {
     let gpu_array = constant(value, Dim4::new(&[1, 1, 1, 1]));
     let mut host_array = &mut vec![0];
     gpu_array.host(&mut host_array);
+
     assert_eq!(value, host_array[0]);
 }
 
@@ -30,6 +31,7 @@ fn can_create_array_with_one_constant_double_value() {
     let gpu_array = constant(value, Dim4::new(&[1, 1, 1, 1]));
     let mut host_array = &mut vec![0.0];
     gpu_array.host(&mut host_array);
+
     assert_eq!(value, host_array[0]);
 }
 
@@ -52,7 +54,6 @@ fn can_create_array_with_n_constant_integer_values() {
     gpu_array.host(&mut host_array);
 
     let expected_array = &mut vec![1, 1, 1];
-    assert_eq!(n, host_array.len());
     assert_eq!(expected_array, host_array);
 }
 
@@ -65,7 +66,6 @@ fn can_create_array_with_n_constant_double_values() {
     gpu_array.host(&mut host_array);
 
     let expected_array = &mut vec![1.0, 1.0, 1.0];
-    assert_eq!(n, host_array.len());
     assert_eq!(expected_array, host_array);
 }
 
@@ -81,54 +81,52 @@ fn can_create_array_with_n_values_from_host() {
 }
 
 #[test]
-fn can_create_array_with_n_x_m_constant_integer_values() {
-    let n = 3;
+fn can_create_array_with_m_x_n_constant_integer_values() {
     let m = 3;
+    let n = 3;
     let value = 1;
-    let gpu_array = constant(value, Dim4::new(&[n as u64, m as u64, 1, 1]));
-    let mut host_array = &mut vec![0; n * m];
+    let gpu_array = constant(value, Dim4::new(&[m as u64, n as u64, 1, 1]));
+    let mut host_array = &mut vec![0; m * n];
     gpu_array.host(&mut host_array);
 
     let expected_array = &mut vec![1, 1, 1, 1, 1, 1, 1, 1, 1];
-    assert_eq!(n * m, host_array.len());
     assert_eq!(expected_array, host_array);
 }
 
 #[test]
-fn can_create_array_with_n_x_m_constant_double_values() {
-    let n = 3;
+fn can_create_array_with_m_x_n_constant_double_values() {
     let m = 3;
+    let n = 3;
     let value = 1.0;
-    let gpu_array = constant(value, Dim4::new(&[n as u64, m as u64, 1, 1]));
-    let mut host_array = &mut vec![0.0; n * m];
+    let gpu_array = constant(value, Dim4::new(&[m as u64, n as u64, 1, 1]));
+    let mut host_array = &mut vec![0.0; m * n];
     gpu_array.host(&mut host_array);
 
     let expected_array = &mut vec![1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0];
-    assert_eq!(n * m, host_array.len());
     assert_eq!(expected_array, host_array);
 }
 
 #[test]
-fn can_create_array_with_n_x_m_values_from_host() {
-    let n = 2;
-    let m = 3;
+fn can_create_array_with_m_x_n_values_from_host() {
+    let m = 2;
+    let n = 3;
     let data = &vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
-    let gpu_array = Array::new(data, Dim4::new(&[n as u64, m as u64, 1, 1]));
-    let mut host_array = &mut vec![0.0; n * m];
+    let gpu_array = Array::new(data, Dim4::new(&[m as u64, n as u64, 1, 1]));
+    let mut host_array = &mut vec![0.0; m * n];
     gpu_array.host(&mut host_array);
 
     assert_eq!(data, host_array);
 }
 
 #[test]
-fn can_create_array_with_n_x_m_uniformly_distributed_values() {
-    let n = 3;
+fn can_create_array_with_m_x_n_uniformly_distributed_values() {
     let m = 3;
-    let gpu_array = randu::<f64>(Dim4::new(&[n as u64, m as u64, 1, 1]));
-    let mut host_array = &mut vec![0.0; n * m];
+    let n = 3;
+    let gpu_array = randu::<f64>(Dim4::new(&[m as u64, n as u64, 1, 1]));
+    let mut host_array = &mut vec![0.0; m * n];
     gpu_array.host(&mut host_array);
 
-    for i in 0..n * m {
+    for i in 0..m * n {
         assert!(host_array[i] >= 0.0);
         assert!(host_array[i] <= 1.0);
     }
@@ -141,16 +139,15 @@ fn can_create_3_x_3_identity_matrix() {
     gpu_array.host(&mut host_array);
 
     let expected_array = &mut vec![1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0];
-    assert_eq!(3 * 3, host_array.len());
     assert_eq!(expected_array, host_array);
 }
 
 #[test]
 fn can_add_matrices() {
-    let n = 2;
-    let m = 3;
-    let a = Array::new(&vec![1.0, 3.0, 1.0, 1.0, 0.0, 0.0], Dim4::new(&[n as u64, m as u64, 1, 1]));
-    let b = Array::new(&vec![0.0, 0.0, 5.0, 7.0, 5.0, 0.0], Dim4::new(&[n as u64, m as u64, 1, 1]));
+    let m = 2;
+    let n = 3;
+    let a = Array::new(&vec![1.0, 3.0, 1.0, 1.0, 0.0, 0.0], Dim4::new(&[m as u64, n as u64, 1, 1]));
+    let b = Array::new(&vec![0.0, 0.0, 5.0, 7.0, 5.0, 0.0], Dim4::new(&[m as u64, n as u64, 1, 1]));
     let c = a + b;
     let mut host_c = &mut vec![0.0; n * m];
     c.host(&mut host_c);
