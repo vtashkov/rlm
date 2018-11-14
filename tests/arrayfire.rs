36fc3fb38bebffb002eb3bc7b502792910307a73
Victor Tashkov <vtashkov@gmail.com>
2018-11-14 22:37:51 +0200

    unit tests refactored

--- tests/arrayfire.rs
+++ tests/arrayfire.rs
@@ -5,21 +5,21 @@ use arrayfire::*;
 #[test]
 fn can_create_array_with_one_constant_value() {
     let value = 1;
-    let a = constant(value, Dim4::new(&[1, 1, 1, 1]));
-    let mut b = &mut vec![0];
-    a.host(&mut b);
-    assert_eq!(value, b[0]);
+    let gpu_array = constant(value, Dim4::new(&[1, 1, 1, 1]));
+    let mut host_array = &mut vec![0];
+    gpu_array.host(&mut host_array);
+    assert_eq!(value, host_array[0]);
 }
 
 #[test]
 fn can_create_array_with_n_constant_values() {
     let n = 3;
     let value = 1;
-    let a = constant(value, Dim4::new(&[n as u64, 1, 1, 1]));
-    let mut b = &mut vec![0; n];
-    a.host(&mut b);
+    let gpu_array = constant(value, Dim4::new(&[n as u64, 1, 1, 1]));
+    let mut host_array = &mut vec![0; n];
+    gpu_array.host(&mut host_array);
 
-    let mut expected = &mut vec![1, 1, 1];
-    assert_eq!(n, b.len());
-    assert_eq!(expected, b);
+    let expected_array = &mut vec![1, 1, 1];
+    assert_eq!(n, host_array.len());
+    assert_eq!(expected_array, host_array);
 }
