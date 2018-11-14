4fe2aa028776dce4f17af8fc5883ddcea788a465
Victor Tashkov <vtashkov@gmail.com>
2018-11-14 22:54:26 +0200

    added tests for double values

--- tests/arrayfire.rs
+++ tests/arrayfire.rs
@@ -3,7 +3,7 @@ extern crate arrayfire;
 use arrayfire::*;
 
 #[test]
-fn can_create_array_with_one_constant_value() {
+fn can_create_array_with_one_constant_integer_value() {
     let value = 1;
     let gpu_array = constant(value, Dim4::new(&[1, 1, 1, 1]));
     let mut host_array = &mut vec![0];
@@ -12,7 +12,16 @@ fn can_create_array_with_one_constant_value() {
 }
 
 #[test]
-fn can_create_array_with_n_constant_values() {
+fn can_create_array_with_one_constant_double_value() {
+    let value = 1.0;
+    let gpu_array = constant(value, Dim4::new(&[1, 1, 1, 1]));
+    let mut host_array = &mut vec![0.0];
+    gpu_array.host(&mut host_array);
+    assert_eq!(value, host_array[0]);
+}
+
+#[test]
+fn can_create_array_with_n_constant_integer_values() {
     let n = 3;
     let value = 1;
     let gpu_array = constant(value, Dim4::new(&[n as u64, 1, 1, 1]));
@@ -20,6 +29,19 @@ fn can_create_array_with_n_constant_values() {
     gpu_array.host(&mut host_array);
 
     let expected_array = &mut vec![1, 1, 1];
+    assert_eq!(n, host_array.len());
+    assert_eq!(expected_array, host_array);
+}
+
+#[test]
+fn can_create_array_with_n_constant_double_values() {
+    let n = 3;
+    let value: f64 = 1.0;
+    let gpu_array = constant(value, Dim4::new(&[n as u64, 1, 1, 1]));
+    let mut host_array = &mut vec![0.0; n];
+    gpu_array.host(&mut host_array);
+
+    let expected_array = &mut vec![1.0, 1.0, 1.0];
     assert_eq!(n, host_array.len());
     assert_eq!(expected_array, host_array);
 }
