64bb181c11520babed0c76592a8288f70e14e5a9
Victor Tashkov <vtashkov@gmail.com>
2018-11-15 21:40:50 +0200

    one variable linear regression algorithm added

--- src/linear_regression.rs
+++ src/linear_regression.rs
@@ -1,7 +1,49 @@
+struct LinearRegressionModel {
+    theta0: f64,
+    theta1: f64,
+}
+
+fn predict(model: &LinearRegressionModel, x: f64) -> f64 {
+    model.theta0 + x * model.theta1
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn all_zero_model_always_predicts_zero() {
+        let model = LinearRegressionModel {
+            theta0: 0.0,
+            theta1: 0.0,
+        };
+
+        assert_eq!(0.0, predict(&model, 0.0));
+        assert_eq!(0.0, predict(&model, 1.0));
+        assert_eq!(0.0, predict(&model, -1.0));
+    }
+
+    #[test]
+    fn zero_one_model_always_predicts_x() {
+        let model = LinearRegressionModel {
+            theta0: 0.0,
+            theta1: 1.0,
+        };
+
+        assert_eq!(0.0, predict(&model, 0.0));
+        assert_eq!(1.0, predict(&model, 1.0));
+        assert_eq!(-1.0, predict(&model, -1.0));
+    }
+
+    #[test]
+    fn one_one_model_always_predicts_x_plus_one() {
+        let model = LinearRegressionModel {
+            theta0: 1.0,
+            theta1: 1.0,
+        };
+
+        assert_eq!(1.0, predict(&model, 0.0));
+        assert_eq!(2.0, predict(&model, 1.0));
+        assert_eq!(0.0, predict(&model, -1.0));
     }
 }
